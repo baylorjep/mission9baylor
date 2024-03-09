589ca95415d347316d62517d93cb797cec744789
Baylor Jeppsen <baylorj@byu.edu>
2024-03-08 22:41:18 -0700

    added name

--- bball/src/App.tsx
+++ bball/src/App.tsx
@@ -3,8 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-
   return (
     <div className="App">
       <header className="App-header">
@@ -18,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Baylor
         </a>
       </header>
     </div>
